070006e8f9f731df60b0f04988ce151fbed37329
samplesurgeon <samplesurgeon@stevevaio.thegrapevine.local>
2010-06-28 23:08:29 +1000

    background positions

--- src/test/js/atomic/tests/PieceMakerTest.js
+++ src/test/js/atomic/tests/PieceMakerTest.js
@@ -1,8 +1,23 @@
 require("../include/include.js");
 
 function testBackgrounds() {
-    var p = P.PieceMaker.make("myimg.jpg", 0);
-    assertEquals("myimg.jpg", p.css("background-image"));
-    assertEquals("no-repeat", p.css("background-repeat"));
-    assertEquals("0% 0%", p.css("background-position"));
+
+    function check(pos, backgroundPos) {
+        var p = P.PieceMaker.make("myimg.jpg", pos);
+        assertEquals("myimg.jpg", p.css("background-image"));
+        assertEquals("no-repeat", p.css("background-repeat"));
+        assertEquals(backgroundPos, p.css("background-position"));
+    }
+
+    check(0, "0% 0%");
+    check(1, "-33% 0%");
+    check(2, "-67% 0%");
+
+    check(3, "0% -33%");
+    check(4, "-33% -33%");
+    check(5, "-67% -33%");
+
+    check(6, "0% -67%");
+    check(7, "-33% -67%");
+    check(8, "-67% -67%");
 }
