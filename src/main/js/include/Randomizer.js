6c1b5807cc9921b2787c6201b847ddfb194e90c3
samplesurgeon <samplesurgeon@stevevaio.thegrapevine.local>
2010-06-29 21:34:34 +1000

    Real randomizer implemented

--- src/main/js/include/Randomizer.js
+++ src/main/js/include/Randomizer.js
@@ -1,8 +1,25 @@
 P.Randomizer = {
-    randomize : function(size) {
+    num : function(x) {
+        var index = Math.floor(Math.random() * x);
+        if (index === x) index--; // prevent (rare) chance of getting x - see https://developer.mozilla.org/en/Core_JavaScript_1.5_Reference/Objects/Math/random
+        return index;
+    },
+
+    /** Returns an array of the numbers [0..size) in a random order. Each number is used exactly once. */
+    array : function(size) {
+        function pick(picked, remaining) {
+            var len = remaining.length;
+            if (len === 0) return picked;
+
+            var index = P.Randomizer.num(len);
+            var chosen = remaining[index];
+            picked.push(chosen);
+            remaining.splice(index, 1);
+            return pick(picked, remaining);
+        }
+
         var max = size + 1;
         var remaining = _.range(max);
-
-        return remaining;    
+        return pick([], remaining);
     }
 };
--- src/test/js/atomic/tests/RandomizerTest.js
+++ src/test/js/atomic/tests/RandomizerTest.js
@@ -3,7 +3,7 @@ require("../include/include.js");
 function test() {
     function check() {
         var expected = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-        var actual = P.Randomizer.randomize(8);
+        var actual = P.Randomizer.array(8);
         actual.sort();
         assertArrayEquals(expected, actual);
     }
