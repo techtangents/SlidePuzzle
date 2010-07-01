609fba3d159b727a393b67ee9527f87db00087f0
techtangents <dylan@techtangents.com>
2010-07-01 20:41:43 +1000

    Refactoring and fixes.

--- src/main/js/include/alien/Arrays.js
+++ src/main/js/include/alien/Arrays.js
@@ -1,5 +1,3 @@
-// array helper
-// FIX This method should be in another project - e.g. TheDLibrary already has this.
 P.Arrays = {
     torso : function(array) {
         return array.slice(0, array.length - 1);
--- src/main/js/include/initialize/Initializer.js
+++ src/main/js/include/initialize/Initializer.js
@@ -1,24 +1,23 @@
 P.Initializer = {
     initialize : function(element, image, rows, cols) {
-        var totalWidth = element.width();
-        var totalHeight = element.height();
-
-        // FIX push the rows/cols throughout the system
-        // FIX pass them in from widget options
-
         var gridInfo = P.GridInfoMaker.make(rows, cols, element);
+        var pieceContainer = P.Initializer.makePieceContainer();
+        element.append(pieceContainer);
+        P.Initializer.addPieces(gridInfo, pieceContainer, image);
+    },
 
+    makePieceContainer : function() {
+        // pieces are absolutely positioned within a relative element.
+        // this makes their coordinates relative to this parent, rather than the document
+        return $("<div />").css("position", "relative");
+    },
+
+    addPieces : function(gridInfo, pieceContainer, image) {
         var picturePieces = P.PieceMaker.array(gridInfo, image);
         var blankPiece = P.PieceMaker.blank(gridInfo);
         var shuffledPieces = P.PieceShuffler.shuffle(picturePieces, blankPiece, P.Randomizer.array);
 
-        // absolutely positioned within a relative element makes the pieces position absolute,
-        //  relative to the parent
-        var pieceContainer = $("<div />").css("position", "relative");
-        element.append(pieceContainer);
-
         P.PieceInserter.insert(gridInfo, pieceContainer, shuffledPieces);
-
         P.EventWirer.wire(picturePieces, blankPiece);
     }
 };
